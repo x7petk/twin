de0c46fdb5dd1496adf99f5ed9d65af7a44957c1
x7petk <x7petk@gmail.com>
2026-01-13 17:10:11 +1300

    updated ci/cd backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "s3" {
-    # These values will be set by deployment scripts
-    # For local development, they can be passed via -backend-config
+    bucket         = "twin-terraform-state-021399177462"
+    key            = "env/dev/terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "twin-terraform-locks"
+    encrypt        = true
   }
 }
