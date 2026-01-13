b8e54886e5c5768a11f1446823d9b763f60ba832
x7petk <x7petk@gmail.com>
2026-01-14 09:01:53 +1300

    updated avatar

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "twin-terraform-state-021399177462"
-    key            = "env/dev/terraform.tfstate"
-    region         = "ap-southeast-2"
-    encrypt        = true
-
-    # если пока оставляешь DynamoDB locking:
-    dynamodb_table = "twin-terraform-locks"
+    # These values will be set by deployment scripts
+    # For local development, they can be passed via -backend-config
   }
 }
