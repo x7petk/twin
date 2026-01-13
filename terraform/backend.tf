3a7604ffaf04d7f97133c5770d5774db280e4022
x7petk <x7petk@gmail.com>
2026-01-13 17:15:36 +1300

    new updates

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,9 @@ terraform {
     bucket         = "twin-terraform-state-021399177462"
     key            = "env/dev/terraform.tfstate"
     region         = "ap-southeast-2"
-    dynamodb_table = "twin-terraform-locks"
     encrypt        = true
+
+    # если пока оставляешь DynamoDB locking:
+    dynamodb_table = "twin-terraform-locks"
   }
 }
